2d6f0a1781d7407a6d69293c715ccf88890b8e26
Fredrik Augustinus Villo <fredrik.villo@gmail.com>
2025-04-07 13:06:59 +0200

    Fungerende HelloWorld webapp

---
+++ TSD2491-oblig-1-261660/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TSD2491_oblig_1_261660.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
